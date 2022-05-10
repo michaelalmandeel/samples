b488336a24ab3ed33deb549ebcf9e687f9e546a6
michaelalmandeel <49460391+michaelalmandeel@users.noreply.github.com>
2022-05-09 22:40:34 -0400

    Update ModularBase64Converter.js

--- ModularBase64Converter.js
+++ ModularBase64Converter.js
@@ -1,25 +1,25 @@
 class ModularBase64Converter
 {
-    computeItoA(i)//convert byte array to an array of base 64 characters using modular arithmatic to compute the indexes of the corrosponding entries 
+    computeItoA(byteArray)//convert byte array to an array of base 64 characters using modular arithmatic to compute the indexes of the corrosponding entries 
     // note that these functions are inverses of eachother
     {
         let charArray = new Array(Uint16Array);
 
-        for(let index = 0; index < i.length*8; index++)
+        for(let index = 0; index < byteArray.length*8; index++)
         {
-            charArray[index/6].at((index+6)%6) = i[index/8].at((index+8)%8);
+            charArray[index/6].at((index+6)%6) = byteArray[index/8].at((index+8)%8);
         }
 
         return charArray;
     }
 
-    computeAtoI(a)//convert array of base 64 characters to byte array using modular arithmatic to compute the indexes of the corrosponding entries 
+    computeAtoI(charArray)//convert array of base 64 characters to byte array using modular arithmatic to compute the indexes of the corrosponding entries 
     {
         let intArray = new Array(Uint8Array);
 
-        for(let index = 0; index < a.length*6; index++)
+        for(let index = 0; index < charArray.length*6; index++)
         {
-            intArray[index/8].at((index+8)%8) = a[index/6].at((index+6)%6);
+            intArray[index/8].at((index+8)%8) = charArray[index/6].at((index+6)%6);
         }
 
         return intArray;
