709fe7900df2e97f602747eaba39819285a2d5bc
michaelalmandeel <49460391+michaelalmandeel@users.noreply.github.com>
2022-05-19 02:03:42 -0400

    Update getHeight.ts

--- getHeight.ts
+++ getHeight.ts
@@ -4,11 +4,11 @@
    //we ensure our calculated height is < max height. the value is to be returned to a style binding.
    // this.listItems refers to a querylist of elementrefs that represent the elements in the list
 
-    let totalContentHeight = 0;//to calculate running total of content height;
-    let previousHeight = 0;//used to record the previous running total
-    let marginHeight = 10;
-    let maxHeight = 480;
-    let numElements = 0;
+    let totalContentHeight:number = 0;//to calculate running total of content height;
+    let previousHeight:number = 0;//used to record the previous running total
+    let marginHeight:number = 10;
+    let maxHeight:number = 480;
+    let numElements:number = 0;
 
     for(let i = 0; i < this.listItems.length; i++)// for each content element
     {
@@ -22,5 +22,7 @@
       }
       numElements++;
     }
+
   this.myHeight = (totalContentHeight+(numElements>0 ? (marginHeight*(numElements-1)):0))+"px";//this.myHeight determines height style binding
+
   }
