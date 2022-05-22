a69a325cd3c95e87f40a4a73c5ccc90af789e1cc
michaelalmandeel <49460391+michaelalmandeel@users.noreply.github.com>
2022-05-22 19:11:59 -0400

    Update ipv4 fragment.cpp

--- ipv4 fragment.cpp
+++ ipv4 fragment.cpp
@@ -13,8 +13,13 @@ class ipv4Fragment
     {
         unsigned short int fragmentSize = (mtu-(frame_hdr_len+datagram_hdr_len)) - (mtu-(frame_hdr_len+datagram_hdr_len))%8; 
         unsigned short int srcTotalLength = 0;
+        try
+        {
         memmove( &srcTotalLength , datagram+total_length_offset+1 , 1 );
         memmove( &srcTotalLength+1 , datagram+total_length_offset , 1 );
+        }
+        catch(){return -1;}
+        
         unsigned short int numFragments = ( (srcTotalLength-datagram_hdr_len)/fragmentSize ) + ( (srcTotalLength-datagram_hdr_len)%fragmentSize > 0 ) ? 1 : 0);
         unsigned short int lastFragmentSize = ( (srcTotalLength-datagram_hdr_len)%fragmentSize > 0 ) ? (srcTotalLength-datagram_hdr_len)%fragmentSize : fragmentSize);
         unsigned short int bufferIndex = 0;
@@ -22,19 +27,20 @@ class ipv4Fragment
         unsigned short int fragmentOffsetVal = (((unsigned short int)datagram[frame_hdr_len+fragment_offset])*256 + ((unsigned short int)datagram[frame_hdr_len+fragment_offset+1])&(0x1FFF))*8;
         unsigned char * header = new unsigned char[frame_hdr_len+datagram_hdr_len+fragmentSize];
        
-        try{
+        try
+        {
         memmove( header , frame_hdr , frame_hdr_len );//append frame header
         memmove( header+frame_hdr_len , datagram , datagram_hdr_len );//append ip header
         memmove( header+frame_hdr_len+total_length_offset+1 , (unsigned char *)&totalLength , 1);//set total length(lower byte)
         memmove( header+frame_hdr_len+total_length_offset , (unsigned char *)&totalLength+1 , 1);//set total length(lower byte)
         header[frame_hdr_len+fragment_offset] = header[frame_hdr_len+fragment_offset]|0x20;//set more flag to 1 by or-ing flags byte with 32    
         for(unsigned short int i = ip_payload_offset;i <= ip_payload_offset+((numFragments-1)*fragmentSize);i += fragmentSize) //construct all frames except the last frame
-        {
+         {
           memmove( buffer+((bufferIndex++)*(frame_hdr_len+datagram_hdr_len+fragmentSize))+frame_hdr_len+datagram_hdr_len , datagram+i , fragmentSize);//copy appropriate segment of datagram payload to the buffer
           header[frame_hdr_len+fragment_offset] = ((unsigned char)((fragmentOffsetVal+(i-ip_payload_offset))/2048))&(header[frame_hdr_len+fragment_offset]&0xE0);//set 1st byte of fragment offset
           header[frame_hdr_len+fragment_offset+1] = (unsigned char)(((fragmentOffsetVal+(i-ip_payload_offset))/8)&00FF);//set second byte of fragment offset
           memmove( buffer+((bufferIndex++)*(frame_hdr_len+datagram_hdr_len+fragmentSize)) , header , frame_hdr_len+datagram_hdr_len );//copy header to buffer
-        }
+         }
         totalLength = lastFragmentSize+datagram_hdr_len;
         memmove( header+frame_hdr_len , datagram , datagram_hdr_len );//append ip header
         memmove( header+frame_hdr_len+total_length_offset+1 , (unsigned char *)&totalLength , 1);//set total length(lower byte)
